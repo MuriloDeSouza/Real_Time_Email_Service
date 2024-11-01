600b7c0f076d6956efcf19ba6ae53c61df25424c
Murilo Prianti <murilo.silva@sou.inteli.edu.br>
2024-11-01 11:56:46 -0300

    adaptando o código para rodar só em node js

--- backend/database.py
+++
@@ -1,7 +0,0 @@
-from supabase import create_client, Client
-
-def conectar_supabase() -> Client:
-    url = "https://ofqnhcrmsxvrlivqxoqh.supabase.co"  # Substitua por sua URL doSupabase
-    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9mcW5oY3Jtc3h2cmxpdnF4b3FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg1MjkyNDgsImV4cCI6MjA0NDEwNTI0OH0.XxXElUoIkUXeux7OqJdUh5IQ9gdLAXHL0boTQ2_yII4"  # Substitua pela sua chave de API do Supabase
-    return create_client(url, key)
-
